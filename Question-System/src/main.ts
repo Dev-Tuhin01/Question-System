03e52158ee2e7de0b7e0ebd2a73f45fa82a861b9
Tuhin <dev.tuhin.1@gmail.com>
2023-02-23 10:17:42 +0530

    Improved code for functionalities for next and previous buttons moved the code to function buttonLoader in qustionLoader.ts to the global scope of main.ts.

--- Question-System/src/main.ts
+++ Question-System/src/main.ts
@@ -17,47 +17,6 @@ let answerSheet: AnswerSheetType[] = [{
 ];
 
 let index: number = 1;
-const numberOfQuestions: number = answerSheet.length;
-
-console.log(numberOfQuestions);
-
 
 QuestionLoader(answerSheet, index);
 
-
-const previousButton: HTMLButtonElement = document.querySelector("#previous") as HTMLButtonElement;
-const checkButton: HTMLButtonElement = document.querySelector("#check") as HTMLButtonElement;
-const nextButton: HTMLButtonElement = document.querySelector("#next") as HTMLButtonElement;
-
-console.log(previousButton);
-console.log(checkButton);
-console.log(nextButton);
-
-answerSheet.forEach(async (ans:AnswerSheetType,index:number) => {
-  console.log(index,ans);
-  
-})
-
-previousButton.addEventListener("click", () => {
-  if (index > -1) {
-    index--;
-    console.log("previous element");
-    QuestionLoader(answerSheet, index);
-    
-  } else {
-    console.error("Limit reached");
-    
-  }
-});
-
-nextButton.addEventListener("click", () => {
-  if (index < numberOfQuestions) {
-    index++;
-    QuestionLoader(answerSheet, index);
-    console.log("next element");
-    
-  } else {
-    console.error("Limit reached");
-    
-  }
-});
--- Question-System/src/qustionLoader.ts
+++ Question-System/src/qustionLoader.ts
@@ -1,7 +1,37 @@
 export interface AnswerSheetType{
     Question: string;
     Answer: string;
-}[]
+}[];
+
+
+export const buttonLoader = (pb:HTMLButtonElement, nb:HTMLButtonElement,index: number, answerSheet:AnswerSheetType[] ) => {
+  
+  console.log(pb, nb);
+  
+  pb.addEventListener("click", () => {
+    
+    if (index > 0) {
+      index--;
+      QuestionLoader(answerSheet, index);
+      console.log("Prevoius element called");
+    } else { 
+      console.log("out of bound");
+    }
+  });
+
+  nb.addEventListener("click", () => {
+    if (index < answerSheet.length - 1) {
+      index++;
+      QuestionLoader(answerSheet, index);
+      console.log("Next element is called");
+    } else {
+      console.log("Out of bound");
+      
+    }
+  });
+
+}
+
 export function QuestionLoader(AnswerSheet: AnswerSheetType[],Index:number) {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id = "Sheet">
@@ -13,6 +43,10 @@ export function QuestionLoader(AnswerSheet: AnswerSheetType[],Index:number) {
     <button type = "button" id = "next">Next</button>
   </div>
 `
+  const pb:HTMLButtonElement = document.querySelector("#previous")!;
+  const nb:HTMLButtonElement = document.querySelector("#next")!;
+  
+  buttonLoader(pb, nb, Index, AnswerSheet);
   
   console.log(Index);
 }
